b360fea0f1b17732100b2847f7435838b3db0b12
DevYuns <ydhun93@gmail.com>
2021-10-04 18:05:33 +0900

    Create basic form page then add test code

--- src/components/pages/Temp.svelte
+++ src/components/pages/Temp.svelte
@@ -1,1 +1,21 @@
+<script lang="ts">
+    let password: string;
+    let passwordConfirm: string;
+
+    $: disabled = (password && passwordConfirm) ? password !== passwordConfirm : true;
+</script>
+
 <h1>This is test</h1>
+<label for="username">username</label>
+<input id="username"/>
+
+<label for="email">email</label>
+<input id="email"/>
+
+<label for="password">password</label>
+<input id="password" type="password" bind:value={password}/>
+
+<label for="password-confirm">password confirm</label>
+<input id="password-confirm" type="password" bind:value={passwordConfirm}/>
+
+<button {disabled}>Sign up</button>
--- src/components/pages/__test__/Temp.test.ts
+++ src/components/pages/__test__/Temp.test.ts
@@ -3,14 +3,88 @@ import '@testing-library/jest-dom';
 import {render, screen} from '@testing-library/svelte';
 
 import Temp from '../Temp.svelte';
+import userEvent from '@testing-library/user-event';
 
 describe('Temp', () => {
   describe('Layout', () => {
     it('has header', () => {
       render(Temp);
-      const page = screen.getByRole('heading', {name: 'This is test'});
+      const comp = screen.getByRole('heading', {name: 'This is test'});
 
-      expect(page).toBeInTheDocument();
+      expect(comp).toBeInTheDocument();
+    });
+
+    it('has username input', () => {
+      render(Temp);
+      const input = screen.getByLabelText('username');
+
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has email input', () => {
+      render(Temp);
+      const input = screen.getByLabelText('email');
+
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has password input', () => {
+      render(Temp);
+      const input = screen.getByLabelText('password');
+
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has password type for password input', () => {
+      render(Temp);
+      const input = screen.getByLabelText('password') as HTMLInputElement;
+
+      expect(input.type).toBe('password');
+    });
+
+    it('has password confirm input', () => {
+      render(Temp);
+      const input = screen.getByLabelText('password confirm');
+
+      expect(input).toBeInTheDocument();
+    });
+
+    it('has password type for password confirm input', () => {
+      render(Temp);
+      const input = screen.getByLabelText(
+        'password confirm',
+      ) as HTMLInputElement;
+
+      expect(input.type).toBe('password');
+    });
+
+    it('has sign up button', () => {
+      render(Temp);
+      const button = screen.getByRole('button', {name: 'Sign up'});
+
+      expect(button).toBeInTheDocument();
+    });
+
+    it('disables the button initially', () => {
+      render(Temp);
+      const button = screen.getByRole('button', {name: 'Sign up'});
+
+      expect(button).toBeDisabled();
+    });
+  });
+
+  describe('Interactions', () => {
+    it('enables the button when the password and password confirm fields have same value', async () => {
+      render(Temp);
+      const password = screen.getByLabelText('password');
+      const passwordConfirm = screen.getByLabelText('password confirm');
+
+      const button = screen.getByRole('button', {name: 'Sign up'});
+
+      await userEvent.type(password, 'password', {delay: 100});
+      await userEvent.type(passwordConfirm, 'password', {delay: 100});
+
+      expect(button).not.toBeDisabled();
     });
   });
 });
